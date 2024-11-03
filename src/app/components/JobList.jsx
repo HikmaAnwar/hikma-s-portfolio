1c80ab9d9550ec288d1e61c241c8bb69c22f4672
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-04 00:02:43 +0300

    fix: update JobList component with new internship details and remove outdated entries for accuracy

--- src/app/components/JobList.jsx
+++ src/app/components/JobList.jsx
@@ -7,11 +7,11 @@ const JobList = () => {
 
   const experienceItems = {
     Amazon: {
-      jobTitle: "Software Development Engineer @",
-      duration: "JUL 2022 - PRESENT",
+      jobTitle: "Web Development Intern @",
+      duration: "JUL 2024 - oct 2024",
       desc: [
-        "Led development of end-to-end region build automation across Route 53 (AWS's DNS web service). This enabled the launch of customer-facing global services in new regions within a day, a significant reduction from the previous time-frame of a month.",
-        "Re-built Route 53's core domain management and DNS systems to provide a better user experience to millions of customers.",
+        "Collaborates with cross-functional teams to develop and optimize innovative solutions.",
+        "Develops and maintain web application named Skillwave an online learning platform using Next.js and Tailwind CSS. ",
       ],
     },
     Wattpad: {
@@ -21,31 +21,6 @@ const JobList = () => {
         "Developed a responsive React web page (the new Story Details) from scratch, both on client and server side, for an app with massive scale (2 billion daily requests).",
         "Iteratively built web experiences for 80 million users across high-traffic pages.",
         "Collaborated with senior engineers and product management following best practices for the full software development life cycle, including coding standards, code reviews, source control management, build processes, testing, and operations.",
-      ],
-    },
-    "University of Toronto": {
-      jobTitle: "Research Engineer @",
-      duration: "MAY 2021 - SEPT 2021",
-      desc: [
-        "Developed and researched an NLP-based framework using state-of-the-art tools like Spacy and Stanza to facilitate the derivation of requirements from health data by leveraging syntactic dependencies, entity-recognition, and rule-based match-making.",
-        "Application selected for DCS Research Award ($4,000) as part of the ”Visualizing Privacy Analysis Results” project led by Professor Marsha Chechik.",
-      ],
-    },
-    Centivizer: {
-      jobTitle: "Software Developer @",
-      duration: "SEPT 2019 - APR 2020",
-      desc: [
-        "Developed interactive and neural-activation technologies to stimulate physical and cognitive functions in order to slow the progression of neurodegenerative disorders.",
-        "Leveraged WebRTC to develop and maintain a Node.js online video-streaming platform in real-time competitive-mode games to research the effects of active stimulation for those suffering from dementia.",
-      ],
-    },
-    "Orange Gate": {
-      jobTitle: "Software Developer Intern @",
-      duration: "MAY 2019 - AUG 2019",
-      desc: [
-        "Developed a Node.js smart home system through Facebook’s Messenger integrated with Bocco sensors and other smart devices (Nest camera, TPLink smart plugs) to derive conclusions about the current state of the home.",
-        "Identified continuous improvements in data quality, design reports, and coding activities, presenting results and findings to internal business stakeholders.",
-        "Relevant technologies/tools used: DialogFlow, Vision, AutoML, Messenger Bot API, MongoDB.",
       ],
     },
   };
@@ -83,10 +58,10 @@ const JobList = () => {
             <div className="text-sm text-gray-400 mb-4">
               {experienceItems[key].duration}
             </div>
-            <ul className="list-disc list-inside text-gray-200 space-y-2">
+            <ul className="list-disc list-inside text-colors-slate space-y-2">
               {experienceItems[key].desc.map((descItem, idx) => (
                 <FadeInSection key={idx} delay={`${idx + 1}00ms`}>
-                  <li>{descItem}</li>
+                  <li className="text-colors-slate">{descItem}</li>
                 </FadeInSection>
               ))}
             </ul>
