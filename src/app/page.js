14fc62a1d357a646aad16e05c107d3312369e2d6
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-03 15:51:02 +0300

    feat: Add react-icon dependency and update page layout for improved styling

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen p-8 pb-20 sm:p-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center min-h-screen  p-8 pb-20 sm:p-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       {/* Ensure NavBar stays fixed at the top */}
       <NavBar />
 
