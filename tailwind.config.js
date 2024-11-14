13dbb1f3b938fcb2fe8df9d3398c5cbf76d140c2
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-14 21:13:00 +0300

    fix: add dark color to Tailwind configuration for enhanced styling options

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         colors: {
           navy: "#0A1A3C", // replace with your navy color value
+          dark: "#070E29",
           lightestSlate: "#DCD5C7", // replace with your lightest-slate color value
           greenBright: "#00ffe1",
           slate: "#a8b2d1",
