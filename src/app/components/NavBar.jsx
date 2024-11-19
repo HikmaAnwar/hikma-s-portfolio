aceba36f99f76bf7220fe4f32ea5de65aeda1f24
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-19 20:50:45 +0300

    fix: remove comments in NavBar component for cleaner code

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -20,12 +20,10 @@ const NavBar = () => {
   return (
     <nav className="fixed top-0 w-full bg-colors-navy text-colors-slate font-bold text-sm z-50 h-10 ">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        {/* Brand - Visible on all screens */}
         <Link href="#" className="text-lg">
           Hikma Anwar anwar tsegaye
         </Link>
 
-        {/* Desktop Links - Hidden on small screens */}
         <div className="hidden md:flex space-x-4">
           <Link href="#intro" className="hover:text-colors-greenBright">
             Home
