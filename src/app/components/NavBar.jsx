41ba8c3a8d9aff77e69ababb7f3dd8ff22c6f977
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-21 17:49:25 +0300

    fix: update NavBar background color to use 'dark' for improved styling

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="fixed top-0 w-full bg-colors-greenBright text-colors-slate font-bold text-sm z-50 h-10 ">
+    <nav className="fixed top-0 w-full bg-colors-dark text-colors-slate font-bold text-sm z-50 h-10 ">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link href="#" className="text-lg">
           Hikma Anwar tsegaye
