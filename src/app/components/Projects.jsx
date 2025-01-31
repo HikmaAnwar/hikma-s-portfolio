586045e799fede3230def4b77823fe33590e66e7
HikmaAnwar <Hikmaanwar89@gmail.com>
2025-01-31 22:15:26 +0300

    refactor: remove "Netview" project entry from Projects component

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -79,12 +79,6 @@ const projects = {
     link: "https://github.com/HikmaAnwar/minim-url",
     open: "https://minim-url.vercel.app",
   },
-  Netview: {
-    desc: "Netview Movie Streaming is a platform that offers a wide range of movies and TV shows.",
-    techStack: "Javascript",
-    link: "https://github.com/HikmaAnwar/netview-movie-streaming",
-    open: "https://netview-movie-streaming.vercel.app/",
-  },
   "Simple Design": {
     desc: "A simple design.",
     techStack: "React",
