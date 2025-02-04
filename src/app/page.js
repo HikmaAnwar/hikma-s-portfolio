165524b63b9f5ee152f82f7f21f4f9b9dca42000
HikmaAnwar <Hikmaanwar89@gmail.com>
2025-02-04 17:53:33 +0300

    refactor: clean up comments and simplify layout structure in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -11,10 +11,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 sm:p-20 gap-16 font-[family-name:var(--font-geist-sans)]">
-      {/* Ensure NavBar stays fixed at the top */}
       <NavBar />
-      {/* Page content with spacing between sections
-      <div className="flex flex-col items-center space-y-2 w-full max-w-screen-lg">
+
+      {/* <div className="flex flex-col items-center space-y-2 w-full max-w-screen-lg">
         <Intro />
         <About />
         <Experience />
