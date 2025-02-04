6bdfe1360b82573b7d0b8ddfd254cbaf670c9307
HikmaAnwar <Hikmaanwar89@gmail.com>
2025-02-04 17:48:45 +0300

    fix: rename 404 component to NotFound for clarity

--- src/app/404.js
+++ src/app/404.js
@@ -1,9 +1,9 @@
-const 404 = () => {
-    return (
-        <div>
-        <h1>404 - Not Found!</h1>
-        </div>
-    );
-    }
-    
-    export default 404;
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Not Found!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
